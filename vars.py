f0bbf31b5f04af0c015889c65de69e25c3978dc0
abcd5977 <abcdef95108@gmail.com>
2025-11-26 13:11:57 +0530

    Create vars.py

---
+++ vars.py
@@ -0,0 +1,7 @@
+api_id = 33516244 #change_with_your_Api_id.
+api_hash = "c1abd0630fcd54d33bc6528ec0955fff" #change_with_your_Api_hash.
+bot_token = "8003550638:AAHPGUxAXWRT2P0VbCC-7Y2nrR2tlVcH-HQ" #change_with_your_bot_token.
+auth_users = [6220046859] #change_with_your_user_id.
+sudo_user = 6220046859 #change_with_your_user_id.
+log_channel = -4873762584 #change_with_your_channel_id.
+txt_channel = -4873762584 #change_with_your_channel_id.
